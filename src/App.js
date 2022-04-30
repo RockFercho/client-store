5778f2c0a98bf393216a63d4f06b873820ee1cbb
Fernando Hurtado <RockFercho@gmail.com>
2022-04-30 00:23:09 -0400

    refactor the path

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  useLocation,
+  useParams,
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+      <Nav />
+
+      <Routes>
+        <Route path="/" exact element={<Home/>} />
+        <Route path="/about" element={<About/>} />
+        <Route path="/contact/:country" element={<Contact/>} />
+        <Route path="*" element={<NotFound/>} />
+      </Routes>
+    </Router>
     </div>
   );
 }
+
+function Nav() {
+    return (
+      <>
+        <Link to="/">Home</Link> | <Link to="/about?name=jamis">About</Link> | <Link to="/contact/us">Contact</Link> | <Link to="/arstrast">arstarst</Link>
+      </>
+    );
+  }
+  
+  function Home() {
+    return <p>Home Page Content</p>;
+  }
+  
+  function About() {
+    var params = new URLSearchParams(useLocation().search);
+    var name = params.get("name");
+    return <p>About Page content for {name}</p>;
+  }
+  
+  function Contact() {
+    var params = useParams();
+    return <p>Contact Page content for {params.country}</p>;
+  }
+  
+  function NotFound() {
+    return <p>Oh No! Page not found.</p>;
+  }
 
 export default App;
