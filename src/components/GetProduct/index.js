b54d6761ee7b199b4d374bcec33726e17209b770
Fernando Hurtado <RockFercho@gmail.com>
2022-05-05 00:10:06 -0400

    dia28 add axios, cambio de estilo de tabla, y obtener los datos del servidor por axios y mostrar en table

--- src/components/GetProduct/index.js
+++ src/components/GetProduct/index.js
@@ -1,11 +1,33 @@
-import * as React from 'react';
+import React, { useEffect, useState} from 'react';
+import { styled } from '@mui/material/styles';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import axios from "axios";
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  '&:nth-of-type(odd)': {
+    backgroundColor: theme.palette.action.hover,
+  },
+  // hide last border
+  '&:last-child td, &:last-child th': {
+    border: 0,
+  },
+}));
 
 function createData(name, calories, fat, carbs, protein) {
   return { name, calories, fat, carbs, protein };
@@ -20,28 +42,35 @@ const rows = [
 ];
 
 export default function GetProduct() {
+
+  const [product, setProduct] =  useState([]);
+
+  useEffect( () => {
+    axios.get("http://localhost:8082/api/producto").then((res) => {
+      console.log("**************** producto **********");
+      console.log(res.data.body);
+      setProduct(res.data.body);
+    })
+  }, []);
   return (
     <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+      <Table sx={{ minWidth: 700 }} aria-label="customized table">
         <TableHead>
           <TableRow>
-            <TableCell>Nombre</TableCell>
-            <TableCell align="right">Precio</TableCell>
-            <TableCell align="right">Vencimiento</TableCell>
+            <StyledTableCell>Nombre</StyledTableCell>
+            <StyledTableCell align="right">Precio</StyledTableCell>
+            <StyledTableCell align="right">Vencimiento</StyledTableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
-            <TableRow
-              key={row.name}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                {row.name}
-              </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-            </TableRow>
+          {product.map((row) => (
+            <StyledTableRow key={row.nombre}>
+              <StyledTableCell component="th" scope="row">
+                {row.nombre}
+              </StyledTableCell>
+              <StyledTableCell align="right">{row.precio}</StyledTableCell>
+              <StyledTableCell align="right">{row.vencimiento}</StyledTableCell>
+            </StyledTableRow>
           ))}
         </TableBody>
       </Table>
