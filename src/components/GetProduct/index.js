cb7dc2dfd0de93b162a60e24cf62de15b00e2860
Fernando Hurtado <RockFercho@gmail.com>
2022-05-11 00:29:17 -0400

    dia 29/ agregado configuracion global y test para busqueda

--- src/components/GetProduct/index.js
+++ src/components/GetProduct/index.js
@@ -7,7 +7,9 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-import axios from "axios";
+import axios from 'axios';
+
+import { URL_SERVER, URL_SERVER_PRODUCTO } from '../../global';
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
   [`&.${tableCellClasses.head}`]: {
@@ -29,51 +31,75 @@ const StyledTableRow = styled(TableRow)(({ theme }) => ({
   },
 }));
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
-
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
-
 export default function GetProduct() {
 
   const [product, setProduct] =  useState([]);
+  const [valueBuscar, setValueBuscar] =  useState();
 
+  const handleSubmit = (e) => {
+    axios
+      .get(URL_SERVER + URL_SERVER_PRODUCTO, 
+        { params: {
+            nombre: valueBuscar 
+          }
+        })
+      .then( (res) => {
+        setProduct(res.data.body);
+      })
+      .catch(err => {
+        console.log('errr', err);
+      })
+      e.preventDefault();
+  }
+  
   useEffect( () => {
-    axios.get("http://localhost:8082/api/producto").then((res) => {
-      console.log("**************** producto **********");
-      console.log(res.data.body);
+    axios.get(URL_SERVER + URL_SERVER_PRODUCTO).then((res) => {
       setProduct(res.data.body);
     })
   }, []);
+
+
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 700 }} aria-label="customized table">
-        <TableHead>
-          <TableRow>
-            <StyledTableCell>Nombre</StyledTableCell>
-            <StyledTableCell align="right">Precio</StyledTableCell>
-            <StyledTableCell align="right">Vencimiento</StyledTableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {product.map((row) => (
-            <StyledTableRow key={row.nombre}>
-              <StyledTableCell component="th" scope="row">
-                {row.nombre}
-              </StyledTableCell>
-              <StyledTableCell align="right">{row.precio}</StyledTableCell>
-              <StyledTableCell align="right">{row.vencimiento}</StyledTableCell>
-            </StyledTableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <div>
+      <div>
+        <h1> LISTA DE PRODUCTOS </h1>
+      </div>
+      <div>
+        <form onSubmit={e => {handleSubmit(e)}}>
+          <label>
+            Buscar:
+            <input type="text"
+                   value={valueBuscar} 
+                   onChange={e => setValueBuscar(e.target.value)}
+            />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+      <div>
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 700 }} aria-label="customized table">
+            <TableHead>
+              <TableRow>
+                <StyledTableCell>Nombre</StyledTableCell>
+                <StyledTableCell align="right">Precio</StyledTableCell>
+                <StyledTableCell align="right">Vencimiento</StyledTableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {product.map((row) => (
+                <StyledTableRow key={row.nombre}>
+                  <StyledTableCell component="th" scope="row">
+                    {row.nombre}
+                  </StyledTableCell>
+                  <StyledTableCell align="right">{row.precio}</StyledTableCell>
+                  <StyledTableCell align="right">{row.vencimiento}</StyledTableCell>
+                </StyledTableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </div>
+    </div>
   );
 }
---
+++ src/global/index.js
@@ -0,0 +1,10 @@
+//const URL_SERVER = 'http://aws.donbosco.com:8082/api';
+const URL_SERVER = 'http://localhost:8082/api';
+const URL_SERVER_PRODUCTO = '/producto';
+const URL_SERVER_USUARIO = '/usuario';
+
+module.exports = {
+  URL_SERVER,
+  URL_SERVER_PRODUCTO,
+  URL_SERVER_USUARIO
+};
